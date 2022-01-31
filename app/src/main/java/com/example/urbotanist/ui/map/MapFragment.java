2879b2610c5cd3a0502091e72f4468ee74548397
Sabrina Hößl <40795517+Rosti97@users.noreply.github.com>
2022-01-31 13:29:54 +0100

    added mapView in MapFragment
    added marker at LatLng(12,12) for testing, commeted old map

--- app/src/main/java/com/example/urbotanist/ui/map/MapFragment.java
+++ app/src/main/java/com/example/urbotanist/ui/map/MapFragment.java
@@ -1,30 +1,35 @@
 package com.example.urbotanist.ui.map;
 
-import androidx.constraintlayout.widget.ConstraintLayout;
 import androidx.lifecycle.ViewModelProvider;
 
 import android.os.Bundle;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
-import androidx.fragment.app.Fragment;
 
 import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.Button;
 
 import com.example.urbotanist.R;
 import com.example.urbotanist.ui.CurrentScreenFragment;
+import com.google.android.gms.maps.CameraUpdateFactory;
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.MapView;
+import com.google.android.gms.maps.OnMapReadyCallback;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.MarkerOptions;
 
 import it.sephiroth.android.library.imagezoom.ImageViewTouch;
 import it.sephiroth.android.library.imagezoom.ImageViewTouchBase;
 
-public class MapFragment extends CurrentScreenFragment {
+public class MapFragment extends CurrentScreenFragment implements OnMapReadyCallback{
 
     private MapViewModel mViewModel;
-    private ImageViewTouch map;
+    //private ImageViewTouch map;
+    private GoogleMap map;
+    private MapView mapView;
 
     public static MapFragment newInstance() {
         return new MapFragment();
@@ -33,12 +38,22 @@ public class MapFragment extends CurrentScreenFragment {
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container,
                              @Nullable Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.map_fragment, container, false);
+        View v = inflater.inflate(R.layout.map_fragment, container, false);
+
+        mapView = (MapView) v.findViewById(R.id.google_map);
+        mapView.onCreate(savedInstanceState);
+
+        //create Map, TODO check permission
+        mapView.getMapAsync(this);
+
+        return v;
     }
+
 
     @Override
     public void onStart() {
         super.onStart();
+        mapView.onStart();
         initGUI();
         Log.d("test", "newmapFragment");
         mViewModel = new ViewModelProvider(this).get(MapViewModel.class);
@@ -49,12 +64,56 @@ public class MapFragment extends CurrentScreenFragment {
     }
 
     private void loadMap(){
-        map = getView().findViewById(R.id.map);
+        //old picture-map
+        /**map = getView().findViewById(R.id.map);
         map.setImageResource(R.drawable.garden_map);
         map.setDisplayType(ImageViewTouchBase.DisplayType.FIT_HEIGHT);
         map.setScrollEnabled(true);
-        map.setQuickScaleEnabled(true);
+        map.setQuickScaleEnabled(true);*/
 
     }
 
+    @Override
+    public void onMapReady(@NonNull GoogleMap googleMap) {
+        map = googleMap;
+        map.getUiSettings().setZoomControlsEnabled(true);
+        map.addMarker(new MarkerOptions().position(new LatLng(12,12)));
+        map.moveCamera(CameraUpdateFactory.newLatLngZoom(new LatLng(12,12), 10));
+    }
+
+    @Override
+    public void onResume() {
+        super.onResume();
+        mapView.onResume();
+    }
+
+    @Override
+    public void onPause() {
+        super.onPause();
+        mapView.onPause();
+    }
+
+    @Override
+    public void onStop() {
+        super.onStop();
+        mapView.onStop();
+    }
+
+    @Override
+    public void onDestroy() {
+        super.onDestroy();
+        mapView.onDestroy();
+    }
+
+    @Override
+    public void onSaveInstanceState(@NonNull Bundle outState) {
+        super.onSaveInstanceState(outState);
+        mapView.onSaveInstanceState(outState);
+    }
+
+    @Override
+    public void onLowMemory() {
+        super.onLowMemory();
+        mapView.onLowMemory();
+    }
 }
