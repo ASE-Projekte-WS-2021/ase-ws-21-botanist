b2485d4af9fc293ee5c5162f58e6245a56ba2475
JJLo1 <82445764+JJLo1@users.noreply.github.com>
2022-02-02 21:47:40 +0100

    replaced fragment move animation with alpha animation

--- app/src/main/java/com/example/urbotanist/ui/CurrentScreenFragment.java
+++ app/src/main/java/com/example/urbotanist/ui/CurrentScreenFragment.java
@@ -7,11 +7,8 @@ import androidx.fragment.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.view.animation.Animation;
-import android.view.animation.TranslateAnimation;
-
-import com.example.urbotanist.R;
-
+import android.view.animation.AccelerateDecelerateInterpolator;
+import android.view.animation.AlphaAnimation;
 
 public abstract class CurrentScreenFragment extends Fragment {
 
@@ -34,10 +31,10 @@ public abstract class CurrentScreenFragment extends Fragment {
     @Override
     public void onStart(){
         super.onStart();
-        TranslateAnimation animation = new TranslateAnimation(Animation.RELATIVE_TO_SELF,0,Animation.RELATIVE_TO_SELF,0,Animation.RELATIVE_TO_SELF,1f,Animation.RELATIVE_TO_SELF,0);
-        getView().setAnimation(animation);
-        animation.setDuration(500);
-        animation.start();
-
+        //TranslateAnimation animation = new TranslateAnimation(Animation.RELATIVE_TO_SELF,0,Animation.RELATIVE_TO_SELF,0,Animation.RELATIVE_TO_SELF,1f,Animation.RELATIVE_TO_SELF,0);
+        AlphaAnimation alphaAnimation = new AlphaAnimation(0f,1f);
+        alphaAnimation.setInterpolator(new AccelerateDecelerateInterpolator());
+        alphaAnimation.setDuration(500);
+        getView().startAnimation(alphaAnimation);
     }
 }
