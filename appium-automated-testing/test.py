dff82922d3e80d45628beeafab4090581204bbf0
JJLo1 <82445764+JJLo1@users.noreply.github.com>
2022-03-07 18:43:48 +0100

    added simple click interaction in appium automated testing

--- appium-automated-testing/test.py
+++ appium-automated-testing/test.py
@@ -3,15 +3,23 @@ import unittest
 from appium import webdriver
 from appium.webdriver.common.appiumby import AppiumBy
 import os
+import time
 
 desired_caps = dict(
     platformName='Android',
     platformVersion='11',
     automationName='uiautomator2',
     deviceName='emulator-5554',
-    app=os.path.abspath('../app/build/outputs/apk/debug/app-debug.apk')
+    app=os.path.abspath('../app/build/outputs/apk/debug/app-debug.apk'),
+    noReset=True
 )
+
+
 print(os.path.abspath('../app/build/outputs/apk/debug/app-debug.apk'))
 driver = webdriver.Remote('http://localhost:4723/wd/hub', desired_caps)
-el = driver.find_element(by=AppiumBy.ACCESSIBILITY_ID, value='item')
+driver.implicitly_wait(20)
+
+time.sleep(10)
+
+el = driver.find_element(by=AppiumBy.ACCESSIBILITY_ID, value='searchButton')
 el.click()
