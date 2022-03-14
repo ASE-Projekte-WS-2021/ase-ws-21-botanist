a6510aae633f9e6667db17aca76f2f8acaea3e95
JJLo1 <82445764+JJLo1@users.noreply.github.com>
2022-03-14 19:07:47 +0100

    removed 2 unused imports

--- app/src/main/java/com/example/urbotanist/MainActivity.java
+++ app/src/main/java/com/example/urbotanist/MainActivity.java
@@ -1,8 +1,6 @@
 package com.example.urbotanist;
 
 import android.annotation.SuppressLint;
-import android.graphics.Color;
-import android.graphics.drawable.ColorDrawable;
 import android.location.Location;
 import android.os.Bundle;
 import android.os.Handler;
