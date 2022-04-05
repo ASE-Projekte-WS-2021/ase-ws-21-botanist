b649cbfedff484ca218462d3abb7d22a81f0f98f
JJLo1 <82445764+JJLo1@users.noreply.github.com>
2022-04-05 20:46:07 +0200

    fixed onboarding crash by setting correct activity theme

--- app/src/main/java/com/example/urbotanist/OnboardingActivity.java
+++ app/src/main/java/com/example/urbotanist/OnboardingActivity.java
@@ -2,18 +2,19 @@ package com.example.urbotanist;
 
 import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
+import androidx.fragment.app.FragmentActivity;
 import com.example.urbotanist.ui.onboarding.OnboardingFragment;
 
-public class OnboardingActivity extends AppCompatActivity {
+public class OnboardingActivity extends FragmentActivity {
 
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.onboarding_activity);
-        if (savedInstanceState == null) {
-            getSupportFragmentManager().beginTransaction()
-                .replace(R.id.container, OnboardingFragment.newInstance())
-                .commitNow();
-        }
+  @Override
+  protected void onCreate(Bundle savedInstanceState) {
+    super.onCreate(savedInstanceState);
+    setContentView(R.layout.onboarding_activity);
+    if (savedInstanceState == null) {
+      getSupportFragmentManager().beginTransaction()
+          .replace(R.id.container, OnboardingFragment.newInstance())
+          .commitNow();
     }
+  }
 }
--- app/src/main/java/com/example/urbotanist/ui/onboarding/OnboardingFragment.java
+++ app/src/main/java/com/example/urbotanist/ui/onboarding/OnboardingFragment.java
@@ -1,5 +1,6 @@
 package com.example.urbotanist.ui.onboarding;
 
+import android.content.Context;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -9,61 +10,64 @@ import androidx.leanback.app.OnboardingSupportFragment;
 
 import com.example.urbotanist.R;
 
-public class OnboardingFragment extends OnboardingSupportFragment{
-    private String[] mTitles;
-    private String[] mDescriptions;
+public class OnboardingFragment extends OnboardingSupportFragment {
 
-    public static OnboardingFragment newInstance() {
-        return new OnboardingFragment();
-    }
+  private String[] mTitles;
+  private String[] mDescriptions;
 
-    @Override
-    public void onAttach(android.app.Activity activity) {
-        super.onAttach(activity);
-        mTitles = new String[]{"1", "2"};
-        mDescriptions = new String[]{"1", "2"};
-        //mDescriptions = getResources().getStringArray(R.array.onboarding_page_descriptions);
-    }
+  public static OnboardingFragment newInstance() {
+    return new OnboardingFragment();
+  }
 
-    @Override
-    protected int getPageCount() {
-        return mTitles.length;
-    }
+  @Override
+  public void onAttach(Context context) {
+    super.onAttach(context);
+    mTitles = new String[]{"1", "2"};
+    mDescriptions = new String[]{"Hello There", "2"};
+    //mDescriptions = getResources().getStringArray(R.array.onboarding_page_descriptions);
+  }
 
-    @Override
-    protected CharSequence getPageTitle(int pageIndex) {
-        return mTitles[pageIndex];
-    }
+  @Override
+  protected int getPageCount() {
+    return mTitles.length;
+  }
 
-    @Override
-    protected CharSequence getPageDescription(int pageIndex) {
-        return mDescriptions[pageIndex];
-    }
+  @Override
+  protected CharSequence getPageTitle(int pageIndex) {
+    return mTitles[pageIndex];
+  }
 
-    @Nullable
-    @Override
-    protected View onCreateBackgroundView(LayoutInflater inflater, ViewGroup container) {
-        return null;
-    }
+  @Override
+  protected CharSequence getPageDescription(int pageIndex) {
+    return mDescriptions[pageIndex];
+  }
 
-    @Nullable
-    @Override
-    protected View onCreateContentView(LayoutInflater inflater, ViewGroup container) {
-        return null;
-    }
+  @Nullable
+  @Override
+  protected View onCreateBackgroundView(LayoutInflater inflater, ViewGroup container) {
+    View v  = inflater.inflate(R.layout.onboarding_fragment, container, false);
+    return v;
+  }
 
-    @Nullable
-    @Override
-    protected View onCreateForegroundView(LayoutInflater inflater, ViewGroup container) {
-        return null;
-    }
+  @Nullable
+  @Override
+  protected View onCreateContentView(LayoutInflater inflater, ViewGroup container) {
 
-    @Override
-    protected void onFinishFragment() {
-        super.onFinishFragment();
-        // User has seen OnboardingSupportFragment, so mark our SharedPreferences
-        // flag as completed so that we don't show our OnboardingSupportFragment
-        // the next time the user launches the app.
+    return null;
+  }
+
+  @Nullable
+  @Override
+  protected View onCreateForegroundView(LayoutInflater inflater, ViewGroup container) {
+    return null;
+  }
+
+  @Override
+  protected void onFinishFragment() {
+    super.onFinishFragment();
+    // User has seen OnboardingSupportFragment, so mark our SharedPreferences
+    // flag as completed so that we don't show our OnboardingSupportFragment
+    // the next time the user launches the app.
         /*
         SharedPreferences.Editor sharedPreferencesEditor =
                 PreferenceManager.getDefaultSharedPreferences(getContext()).edit();
@@ -74,6 +78,7 @@ public class OnboardingFragment extends OnboardingSupportFragment{
         sharedPreferencesEditor.apply();
 
          */
-    }
+    getActivity().finish();
+  }
 
 }
